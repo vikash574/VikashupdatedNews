89232df17b317821691036b77cfc367784fe1d48
vikash574 <133881249+vikash574@users.noreply.github.com>
2024-10-24 10:50:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Newsapp from './Components/Newsapp';
+
+function App() {
+  return (
+    <div className="App">
+     <Newsapp/>
+    </div>
+  );
+}
+
+export default App;
